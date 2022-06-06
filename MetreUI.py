0e5b8cfdec96f56c52292d94e08c20a0beeff2a9
apimetre <73803273+apimetre@users.noreply.github.com>
2022-06-06 15:07:48 -0700

    Update MetreUI.py

--- MetreUI.py
+++ MetreUI.py
@@ -1,4 +1,4 @@
-DEBUG = True
+DEBUG = False
 # REMOVE TRY-EXCEPT
 ## Python imports
 import os
@@ -375,7 +375,7 @@ class MainView(ui.View):
             self.dt = dt_
             global wait_for_et
         def etoh_alert(self):
-            print(self.os.path.exits(self.cwd) + '/' + 'etoh_console.pyui')
+            print(os.path.exits(self.cwd) + '/' + 'etoh_console.pyui')
             self.etoh_console = ui.load_view('etoh_console')
             self.vx.add_subview(self.etoh_console)
             self.etoh_console.center = self.vx.bounds.center()
